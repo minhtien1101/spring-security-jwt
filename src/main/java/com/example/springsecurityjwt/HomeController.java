2e38f0b2a2f5b2c24011eb452c13cd261b71e11e
phiminhtien <pmt11112001@gmail.com>
2022-09-19 10:14:23 +0700

    spring jwt

---
+++ src/main/java/com/example/springsecurityjwt/HomeController.java
@@ -0,0 +1,43 @@
+package com.example.springsecurityjwt;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private JwtUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = {"/home", "/"})
+    public String home() {
+        return "Welcome!";
+    }
+
+    @PostMapping(value = "/authenticate", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID CREDENTIAL", e);
+        }
+        final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUsername());
+        final String token = jwtUtility.generateToken(userDetails);
+
+        return new JwtResponse(token);
+    }
+}
